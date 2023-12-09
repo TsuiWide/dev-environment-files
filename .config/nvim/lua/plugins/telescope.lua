ec6842465619929fa97ff4d7eba5bcb862f90c67
TsuiWide <xukuan89@163.com>
2023-12-09 22:58:25 +0800

    add telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
   branch = "0.1.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    --{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
@@ -25,7 +25,7 @@ return {
       },
     })
 
-    telescope.load_extension("fzf")
+    --telescope.load_extension("fzf")
 
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
